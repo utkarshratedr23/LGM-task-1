83169f8bf0abdb582174dd3545fce5858db6843e
utkarsh bhargava <bhargavautkarsh074@gmail.com>
2023-06-28 22:08:59 +0530

    Fileupload
    task 1 of LGM internship

---
+++ todolist-webapp/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css'
+import TodoList from "./TodoList";
+//Todo
+const App=()=>{
+return<>
+<div className="app">
+ 
+    <TodoList/>
+
+</div>
+</>
+};
+
+export default App;
